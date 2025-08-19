8cef7461ab950072773ede4da372fc83a8c827fb
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2025-08-19 21:03:34 +0300

    Add files via upload

---
+++ EmpSystem/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace EmpSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txt_job = new TextBox();
+            txt_name = new TextBox();
+            txt_salary = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            cbx_name = new ComboBox();
+            label5 = new Label();
+            Save = new Button();
+            Delete = new Button();
+            Edit = new Button();
+            src_name = new TextBox();
+            label6 = new Label();
+            button4 = new Button();
+            Enter = new Button();
+            pic = new PictureBox();
+            button1 = new Button();
+            dtg = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dtg).BeginInit();
+            SuspendLayout();
+            // 
+            // txt_job
+            // 
+            txt_job.Location = new Point(127, 145);
+            txt_job.Name = "txt_job";
+            txt_job.Size = new Size(228, 27);
+            txt_job.TabIndex = 1;
+            // 
+            // txt_name
+            // 
+            txt_name.Location = new Point(127, 102);
+            txt_name.Name = "txt_name";
+            txt_name.Size = new Size(228, 27);
+            txt_name.TabIndex = 2;
+            // 
+            // txt_salary
+            // 
+            txt_salary.Location = new Point(127, 189);
+            txt_salary.Name = "txt_salary";
+            txt_salary.Size = new Size(228, 27);
+            txt_salary.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(56, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Salary";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(56, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Job";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(56, 109);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Name";
+            // 
+            // cbx_name
+            // 
+            cbx_name.FormattingEnabled = true;
+            cbx_name.Location = new Point(105, 357);
+            cbx_name.Name = "cbx_name";
+            cbx_name.Size = new Size(294, 28);
+            cbx_name.TabIndex = 8;
+            cbx_name.SelectedIndexChanged += cbx_name_SelectedIndexChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(50, 360);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Name";
+            label5.Click += label5_Click;
+            // 
+            // Save
+            // 
+            Save.Location = new Point(105, 422);
+            Save.Name = "Save";
+            Save.Size = new Size(94, 29);
+            Save.TabIndex = 10;
+            Save.Text = "Save";
+            Save.UseVisualStyleBackColor = true;
+            Save.Click += Save_Click;
+            // 
+            // Delete
+            // 
+            Delete.Location = new Point(305, 422);
+            Delete.Name = "Delete";
+            Delete.Size = new Size(94, 29);
+            Delete.TabIndex = 11;
+            Delete.Text = "Delete";
+            Delete.UseVisualStyleBackColor = true;
+            Delete.Click += Delete_Click;
+            // 
+            // Edit
+            // 
+            Edit.Location = new Point(205, 422);
+            Edit.Name = "Edit";
+            Edit.Size = new Size(94, 29);
+            Edit.TabIndex = 12;
+            Edit.Text = "Edit";
+            Edit.UseVisualStyleBackColor = true;
+            Edit.Click += Edit_Click;
+            // 
+            // src_name
+            // 
+            src_name.Location = new Point(515, 433);
+            src_name.Name = "src_name";
+            src_name.Size = new Size(271, 27);
+            src_name.TabIndex = 13;
+            src_name.TextChanged += src_name_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(515, 399);
+            label6.Name = "label6";
+            label6.Size = new Size(53, 20);
+            label6.TabIndex = 14;
+            label6.Text = "Search";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(810, 433);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 15;
+            button4.Text = "Show";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Enter
+            // 
+            Enter.Location = new Point(127, 245);
+            Enter.Name = "Enter";
+            Enter.Size = new Size(94, 29);
+            Enter.TabIndex = 18;
+            Enter.Text = "Enter";
+            Enter.UseVisualStyleBackColor = true;
+            Enter.Click += button5_Click;
+            // 
+            // pic
+            // 
+            pic.Location = new Point(566, 121);
+            pic.Name = "pic";
+            pic.Size = new Size(291, 246);
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic.TabIndex = 19;
+            pic.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(261, 245);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 20;
+            button1.Text = "Add Image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dtg
+            // 
+            dtg.AllowUserToDeleteRows = false;
+            dtg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dtg.BackgroundColor = Color.DodgerBlue;
+            dtg.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtg.Location = new Point(137, 482);
+            dtg.Name = "dtg";
+            dtg.RowHeadersWidth = 51;
+            dtg.Size = new Size(720, 102);
+            dtg.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.AliceBlue;
+            ClientSize = new Size(977, 611);
+            Controls.Add(dtg);
+            Controls.Add(button1);
+            Controls.Add(pic);
+            Controls.Add(Enter);
+            Controls.Add(button4);
+            Controls.Add(label6);
+            Controls.Add(src_name);
+            Controls.Add(Edit);
+            Controls.Add(Delete);
+            Controls.Add(Save);
+            Controls.Add(label5);
+            Controls.Add(cbx_name);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txt_salary);
+            Controls.Add(txt_name);
+            Controls.Add(txt_job);
+            Name = "Form1";
+            Text = "Employee";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dtg).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txt_job;
+        private TextBox txt_name;
+        private TextBox txt_salary;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox cbx_name;
+        private Label label5;
+        private Button Save;
+        private Button Delete;
+        private Button Edit;
+        private TextBox src_name;
+        private Label label6;
+        private Button button4;
+        private Button Enter;
+        private PictureBox pic;
+        private Button button1;
+        private DataGridView dtg;
+    }
+}
---
+++ EmpSystem/Form1.cs
@@ -0,0 +1,389 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EmpSystem
+{
+
+    public partial class Form1 : Form
+    {
+        const string connectionString = "Data Source=MEMO;Initial Catalog=TestDB;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        public SqlConnection conn = new SqlConnection(connectionString);
+        byte[] imageBytes;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                string name = txt_name.Text;
+                string job = txt_job.Text;
+                double salary = double.Parse(txt_salary.Text);
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO employees (name, job, salary,Photo) VALUES (@name, @job, @salary,@photo)", conn);
+
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@job", job);
+                cmd.Parameters.AddWithValue("@salary", salary);
+                cmd.Parameters.Add("@photo", SqlDbType.VarBinary).Value = imageBytes;
+
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    MessageBox.Show("Record inserted successfully ✅");
+                }
+                else
+                {
+                    MessageBox.Show("No data was inserted ❌");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid number for the salary!", "Format Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Database error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An unexpected error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+
+        }
+        private void cbx_name_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Photo FROM Employees WHERE Name = @item", conn);
+                cmd.Parameters.AddWithValue("@item", cbx_name.SelectedItem.ToString());
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    if (dr["Photo"] != DBNull.Value)
+                    {
+                        byte[] imgData = (byte[])dr["Photo"];
+                        using (MemoryStream ms = new MemoryStream(imgData))
+                        {
+                            pic.Image = Image.FromStream(ms);
+                            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No image found for this employee.");
+                        pic.Image = null;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Employee not found.");
+                }
+
+                dr.Close();
+            }
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadEmployeeNames();
+
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = 5000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            LoadEmployeeNames();
+        }
+
+
+        private void LoadEmployeeNames()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                    conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT Name FROM Employees where IsDeleted = 0", conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                cbx_name.Items.Clear(); 
+
+                while (dr.Read())
+                {
+                    cbx_name.Items.Add(dr["Name"].ToString());
+                }
+
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                    conn.Close();
+            }
+        }
+
+        private void Edit_Click(object sender, EventArgs e)
+        {
+
+            string oldName = (string)cbx_name.SelectedItem;
+
+            if (MessageBox.Show("Update Name?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string newName = Interaction.InputBox("Enter new name:", "Input Required", oldName);
+
+                if (!string.IsNullOrWhiteSpace(newName))
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("UPDATE Employees SET Name = @newName WHERE Name = @oldName", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@newName", newName);
+                            cmd.Parameters.AddWithValue("@oldName", oldName);
+
+                            int rows = cmd.ExecuteNonQuery();
+                            MessageBox.Show($"{rows} row(s) updated successfully.");
+                            oldName = newName; 
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Update cancelled. No name entered.");
+                }
+            }
+
+            if (MessageBox.Show("Update Salary?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string salaryInput = Interaction.InputBox("Enter new salary:", "Input Required", "0");
+
+                if (decimal.TryParse(salaryInput, out decimal newSalary))
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("UPDATE Employees SET Salary = @salary WHERE Name = @name", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@salary", newSalary);
+                            cmd.Parameters.AddWithValue("@name", oldName);
+
+                            int rows = cmd.ExecuteNonQuery();
+                            MessageBox.Show($"{rows} row(s) updated successfully.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Invalid salary entered.");
+                }
+            }
+
+            if (MessageBox.Show("Update Job?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string newJob = Interaction.InputBox("Enter new job:", "Input Required", "");
+
+                if (!string.IsNullOrWhiteSpace(newJob))
+                {
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("UPDATE Employees SET Job = @job WHERE Name = @name", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@job", newJob);
+                            cmd.Parameters.AddWithValue("@name", oldName);
+
+                            int rows = cmd.ExecuteNonQuery();
+                            MessageBox.Show($"{rows} row(s) updated successfully.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Update cancelled. No job entered.");
+                }
+            }
+
+            if (MessageBox.Show("Update Image?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image Files (*.jpg; *.png; *.gif)|*.jpg;*.png;*.gif";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    byte[] imageBytes = File.ReadAllBytes(openFileDialog.FileName);
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        using (SqlCommand cmd = new SqlCommand("UPDATE Employees SET Photo = @photo WHERE Name = @name", conn))
+                        {
+                            cmd.Parameters.Add("@photo", SqlDbType.VarBinary).Value = imageBytes;
+                            cmd.Parameters.AddWithValue("@name", oldName);
+
+                            int rows = cmd.ExecuteNonQuery();
+                            MessageBox.Show($"{rows} row(s) updated successfully.");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg; *.png; *.gif)|*.jpg;*.png;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pic.Image = Image.FromFile(openFileDialog.FileName);
+                pic.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                string imagePath = openFileDialog.FileName;
+
+                imageBytes = File.ReadAllBytes(imagePath);
+
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=MEMO;Initial Catalog=TestDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("UPDATE Employees SET IsDeleted = 1 WHERE Name = @item", conn);
+
+                string item = cbx_name.SelectedItem.ToString();
+                cmd.Parameters.AddWithValue("@item", item);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                MessageBox.Show($"{rowsAffected} row(s) updated successfully.");
+            }
+        }
+
+        private void src_name_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string name = src_name.Text.ToString();
+                SqlDataAdapter da = new SqlDataAdapter("SELECT Name, Job, Salary, Photo FROM Employees WHERE IsDeleted = 0 AND Name = @name", conn);
+                da.SelectCommand.Parameters.AddWithValue("@name", name);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    DataTable dtDisplay = dt.DefaultView.ToTable(false, "Name", "Job", "Salary");
+                    dtg.DataSource = dtDisplay;
+
+                    dtg.AllowUserToAddRows = false;
+
+                    dtg.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+                    dtg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                    dtg.ScrollBars = ScrollBars.None;
+
+                    dtg.Height = dtg.Rows.GetRowsHeight(DataGridViewElementStates.Visible)
+                                  + dtg.ColumnHeadersHeight;
+
+                    byte[] imageBytes = (byte[])dt.Rows[0]["Photo"];
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        pic.Image = Image.FromStream(ms);
+                        pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("No data found.");
+                    dtg.DataSource = null;
+                    pic.Image = null;
+                }
+            }
+        }
+        private void ResetForm()
+        {
+            if (txt_name != null) txt_name.Clear();
+            if (txt_job != null) txt_job.Clear();
+            if (txt_salary != null) txt_salary.Clear();
+
+            //if (cbx_name != null) cbx_name.SelectedIndex = -1;
+
+            if (dtg != null) dtg.ClearSelection();
+            if (src_name != null) src_name.Clear();
+
+           // if (pic != null) pic.Image = null;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                ResetForm(); 
+
+                MessageBox.Show("Saved Successfully!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while saving: " + ex.Message,
+                    "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
